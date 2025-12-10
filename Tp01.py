13c2663c08b0b568746e0e742a35998a196ad8a8
zizi-cyber-hub <szoulikhamounira@gmail.com>
2025-12-10 20:09:42 +0100

    Sahi zoulikha mounira

--- Tp01.py
+++ Tp01.py
@@ -1,1 +1,13 @@
+# NomDuChefDeProjet, Sahi Zoulikha Mounira, Master 1 Microbiologie Fondamentale , 10/12/2025)
+# Nom du Membre 1 : Nedjar Douaa
+# Nom du Membre 2 : Zeggai Meriem
+# Nom du Membre 3 : Hachim El Chaima
+# Nom du Membre 4 : Benabdallah soumia
+
+import pandas as pd
+
+
+
+
+
 

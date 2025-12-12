5e601118a7ef241ba3083e3a4a02282bc98b4731
zizi-cyber-hub <szoulikhamounira@gmail.com>
2025-12-12 15:21:21 +0100

    Sahi Zoulikha Mounira

--- Tp01.py
+++ Tp01.py
@@ -68,7 +68,7 @@ print(f"Écart-type de la longueur : {std_longueur:.2f}")
 print("\n# 8) Sauvegarde du DataFrame dans un fichier CSV")
 # Sauvegarder le Dataframe dans un fichier CSV (sans l’index)
 file_name = "analyse_séquences_adn_final.csv"
-df.to_csv(file_name, index=false)
+df.to_csv(file_name, index=False)
 print("Le tableau final a ètè Sauvegardè dans '{file_name}' avec succès.")
 
 
